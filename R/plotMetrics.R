79e11e1683b912a42dec80e5466703a1ee1b34d8
Sophie Castel <sophiecastel@trentu.ca>
2019-09-24 16:39:32 -0400

    Adjusted default parameters to be all-inclusive. Changed "metrics" to "criteria"

--- R/plotMetrics.R
+++ R/plotMetrics.R
@@ -7,9 +7,13 @@
 #' @param p A vector of 'proportion missings' of interest (must be a subset of agEval)
 #' @param g A vector of 'gap length' of interest (must be a subset of agEval)
 #' @param m A vector of interpolation methods of interest (must be a subset of agEval)
-#' @param metrics A character vector of performance metrics of interest
+#' @param crit A character vector of performance metrics of interest
 
-plotMetrics <- function(agEval,d,p,m,g,metrics = c("MSE","MAPE","abs_differences")){
+plotMetrics <- function(agEval,d=1:length(agEval),
+                        p=1:length(agEval[[1]]),
+                        m=1:length(agEval[[1]][[1]][[1]]),
+                        g=1:length(agEval[[1]][[1]]),
+                        crit){
 
   require(ggplot2)
   
@@ -17,7 +21,7 @@ D <- length(d)
 P <- length(p)
 M <- length(m)
 G <- length(g)
-Met <- length(metrics)
+C <- length(crit)
 
 criterion <- rownames(agEval[[1]][[1]][[1]][[1]])
 maximize <- c(1,1,rep(0,11),1,rep(0,3)) # 1 = yes, 0 = no
@@ -32,49 +36,49 @@ best <- data.frame(criterion = criterion,
 metDF <- lapply(plotList <- vector(mode = 'list', D),function(x)
   lapply(plotList <- vector(mode = 'list', P),function(x) 
     lapply(plotList <- vector(mode = 'list', G),function(x) 
-      x<-vector(mode='list',Met))))
+      x<-vector(mode='list',C))))
 
 metPlot <- metDF
 
   for(vd in 1:D){
     for(vp in 1:P){
         for(vg in 1:G){
-              for(met in 1:Met){
+              for(s in 1:C){
                 
                 mean <- numeric(M)
                 sd <- numeric(M)
                 method <- numeric(M)
-                metric <- numeric(M)
+                criteria <- numeric(M)
                 
                 for(vm in 1:M){
-                mean[vm] <- agEval[[d[vd]]][[p[vp]]][[g[vg]]][[m[vm]]][metrics[met],"mean"]
-                sd[vm] <- agEval[[d[vd]]][[p[vp]]][[g[vg]]][[m[vm]]][metrics[met],"sd"]
-                method[vm] <- as.character(agEval[[d[vd]]][[p[vp]]][[g[vg]]][[m[vm]]][metrics[met],"method"])
-                metric[vm] <- metrics[met]
+                mean[vm] <- agEval[[d[vd]]][[p[vp]]][[g[vg]]][[m[vm]]][crit[s],"mean"]
+                sd[vm] <- agEval[[d[vd]]][[p[vp]]][[g[vg]]][[m[vm]]][crit[s],"sd"]
+                method[vm] <- as.character(agEval[[d[vd]]][[p[vp]]][[g[vg]]][[m[vm]]][crit[s],"method"])
+                criteria[vm] <- crit[s]
               }
           
-            metDF[[vd]][[vp]][[vg]][[met]] <-   data.frame(mean = mean, sd = sd, method = method, metric = metric)
+            metDF[[vd]][[vp]][[vg]][[s]] <-   data.frame(mean = mean, sd = sd, method = method, criteria = criteria)
       
-            metPlot[[vd]][[vp]][[vg]][[met]] <- ggplot(metDF[[vd]][[vp]][[vg]][[met]],aes(x=method,y=mean)) +
+            metPlot[[vd]][[vp]][[vg]][[s]] <- ggplot(metDF[[vd]][[vp]][[vg]][[s]],aes(x=method,y=mean)) +
 
               geom_col() + 
               
-              geom_bar(data=subset(metDF[[vd]][[vp]][[vg]][[met]], 
-                                   eval(parse(text = paste("mean==",best[(criterion==metrics[met]),'optimal'],"(mean)", sep = "")))), 
+              geom_bar(data=subset(metDF[[vd]][[vp]][[vg]][[s]], 
+                                   eval(parse(text = paste("mean==",best[(criterion==crit[s]),'optimal'],"(mean)", sep = "")))), 
                        aes(method, mean),
                        fill="mistyrose2", stat="identity") + 
               
               geom_errorbar(aes(ymin = mean-sd, ymax = mean+sd), width=0.2)+
             
-              ggtitle(paste(metrics[met],", optimal = ",best[(criterion == metrics[met]),'optimal'],sep=""),
+              ggtitle(paste(crit[s],", optimal = ",best[(criterion == crit[s]),'optimal'],sep=""),
                       subtitle = paste("D",d[vd],", p=",prop_vec[p[vp]],", g=",gap_vec[g[vg]],sep=""))+
               labs(y="value", x = "")+ 
               theme(axis.text.x = element_text(angle = 45, hjust = 1))
               }
           
           
-          names(metDF[[vd]][[vp]][[vg]]) <- metrics
-          names(metPlot[[vd]][[vp]][[vg]]) <- metrics
+          names(metDF[[vd]][[vp]][[vg]]) <- crit
+          names(metPlot[[vd]][[vp]][[vg]]) <- crit
         }
       names(metDF[[vd]][[vp]]) <- names(agEval[[1]][[1]])[g]
       names(metPlot[[vd]][[vp]]) <- names(agEval[[1]][[1]])[g]
@@ -98,7 +102,7 @@ flattenlist <- function(x){
   }
 }
 
-plot.grid <- do.call(grid.arrange, args = list(grobs = flattenlist(metPlot), ncol = Met, nrow = P*G*D))
+plot.grid <- do.call(grid.arrange, args = list(grobs = flattenlist(metPlot), ncol = C, nrow = P*G*D))
 
 return(plot.grid)
 }
