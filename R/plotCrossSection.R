21c3035630ed9b6b06204bcb9ce2ef7976b2098e
Sophie Castel <sophiecastel@trentu.ca>
2019-12-13 19:05:07 -0500

    Fixed bug by adding "group" argument to plot layer, generated a matrix of quantiles. Have yet to decide what to do with these.

--- R/plotCrossSection.R
+++ R/plotCrossSection.R
@@ -1,11 +1,13 @@
 #' Plot Cross Section
 #' 
 #' Function to generate cross-sectional plots to visualize changes in the performance metrics of interest
-#' as the proportion of missing data and gap width increase. Metrics are aggregated across K simulations in each p,g,d,m specification. \cr
-#' The x-axis represents p, the proportion of missing data.\cr 
-#' The y-axis represents g, the gap width.\cr
-#' The z-axis represents the value of the performance metric of interest.
-#' The user decides which variable to show with respect to the z-axis.
+#' as the proportion of missing data and gap width increase. 
+#' Metrics are aggregated across K simulations in each p,g,d,m specification, and plotted with respect to 
+#' a particular variable (p or g), collapsing across the other.  Middle line is the median with respect to f, upper
+#' and lower ribbon limits are Q1 and Q3, respectively. \cr
+#' The x-axis represents p or g, the proportion of missing data, or gap width.\cr 
+#' The y-axis represents the value of the performance metric of interest, collapsed (median) across the `other variable'.
+#' The user decides which variable to show on the x-axis.
 #' 
 #' @param d A vector of the indexes of the datasets of interest
 #' @param m A vector of the interpolation methods of interest (maximum of 5)
@@ -43,10 +45,15 @@ plotCS <- function(d=1:length(agEval),
   z_list <- lapply(z_list <- vector(mode = 'list', C),function(x)
     lapply(z_list <- vector(mode = 'list', M),function(x) 
       x <- vector(mode = 'list', D)))
+  q2.5_list <- z_list
+  q97.5_list <- z_list
   
   critMat <- matrix(nrow=length(agEval[[1]]),ncol=length(agEval[[1]][[1]]))
   rownames(critMat) <- prop_vec_names
   colnames(critMat) <- gap_vec_names
+  
+  q2.5mat <- critMat
+  q97.5mat <- critMat
   
   data_list_names <- numeric(D)
   method_list_names <- numeric(M)
@@ -57,17 +64,29 @@ plotCS <- function(d=1:length(agEval),
         for(p in 1:P){
           for(g in 1:G){
             critMat[p,g] <- agEval[[d[vd]]][[p]][[g]][[m[vm]]][crit[s],f]
+            q2.5mat[p,g] <- agEval[[d[vd]]][[p]][[g]][[m[vm]]][crit[s],"q2.5"]
+            q97.5mat[p,g] <- agEval[[d[vd]]][[p]][[g]][[m[vm]]][crit[s],"q97.5"]
+            
             method_list_names[vm] <- as.character(agEval[[d[vd]]][[p]][[g]][[m[vm]]][crit[s], "method"]) 
           }
         }
         z_list[[s]][[vm]][[vd]] <- critMat
+        q2.5_list[[s]][[vm]][[vd]] <- q2.5mat
+        q97.5_list[[s]][[vm]][[vd]] <- q97.5mat
+        
         data_list_names[vd] <- paste("D",d[vd],sep="") 
       }
       names(z_list[[s]][[vm]]) <- data_list_names
+      names(q2.5_list[[s]][[vm]]) <- data_list_names
+      names(q97.5_list[[s]][[vm]]) <- data_list_names
     }
     names(z_list[[s]]) <- method_list_names
+    names(q2.5_list[[s]]) <- method_list_names
+    names(q97.5_list[[s]]) <- method_list_names
   }
   names(z_list) <- crit
+  names(q2.5_list) <- crit
+  names(q97.5_list) <- crit
   
   ## Generating a list of plots
   
@@ -99,24 +118,26 @@ plotCS <- function(d=1:length(agEval),
         for(vm in 1:(M-1)){
           z[vm] <- paste("geom_ribbon(data = data.frame(fun(data[[",s,"]][[",vm,"]][[",vd,"]])), 
                                                    aes(x = axx, ymin = apply(fun(data[[",s,"]][[",vm,"]][[",vd,"]]),1,min),
-                                                   ymax = apply(fun(data[[",s,"]][[",vm,"]][[",vd,"]]),1,max)), 
+                                                   ymax = apply(fun(data[[",s,"]][[",vm,"]][[",vd,"]]),1,max),
+                                                                                group = ",vm,"), 
                                                    fill = colorList[",vm,"], alpha = 0.4) + 
 
                           geom_line(data = data.frame(fun(data[[",s,"]][[",vm,"]][[",vd,"]])),
                             colour = colorList[",vm,"],
                             aes(x = axx, y = apply(fun(data[[",s,"]][[",vm,"]][[",vd,"]]),1,median),
-                            col = names(data[[",s,"]])[",vm,"])) + ",sep="") 
+                            col = names(data[[",s,"]])[",vm,"], group = ",vm,")) + ",sep="") 
         }
         
         z[M] <- paste("geom_ribbon(data = data.frame(fun(data[[",s,"]][[",M,"]][[",vd,"]])), 
                                                    aes(x = axx, ymin = apply(fun(data[[",s,"]][[",M,"]][[",vd,"]]),1,min),
-                                                   ymax = apply(fun(data[[",s,"]][[",M,"]][[",vd,"]]),1,max)),
+                                                   ymax = apply(fun(data[[",s,"]][[",M,"]][[",vd,"]]),1,max),
+                                                                              group = ",M,"), 
                                                    fill = colorList[",M,"], alpha = 0.4) + 
                       
                       geom_line(data = data.frame(fun(data[[",s,"]][[",M,"]][[",vd,"]])),
                       colour = colorList[",M,"],
                       aes(x = axx, y = apply(fun(data[[",s,"]][[",M,"]][[",vd,"]]),1,median), 
-                      col = names(data[[",s,"]])[",M,"]))",sep="")
+                      col = names(data[[",s,"]])[",M,"], group = ",M,"))",sep="")
       
         z <- paste(z, collapse = "")
         
@@ -163,24 +184,26 @@ plotCS <- function(d=1:length(agEval),
         for(vd in 1:(D-1)){
           z[vd] <- paste("geom_ribbon(data = data.frame(fun(data[[",s,"]][[",vm,"]][[",vd,"]])), 
                                                    aes(x = axx, ymin = apply(fun(data[[",s,"]][[",vm,"]][[",vd,"]]),1,min),
-                                                   ymax = apply(fun(data[[",s,"]][[",vm,"]][[",vd,"]]),1,max)), 
+                                                   ymax = apply(fun(data[[",s,"]][[",vm,"]][[",vd,"]]),1,max),
+                                                                  group = ",vd,"), 
                                                    fill = colorList[",vd,"], alpha = 0.4) + 
 
                           geom_line(data = data.frame(fun(data[[",s,"]][[",vm,"]][[",vd,"]])),
                           colour = colorList[",vd,"],
                           aes(x = axx, y = apply(fun(data[[",s,"]][[",vm,"]][[",vd,"]]),1,median),
-                          col = names(data[[",s,"]][[",vm,"]])[",vd,"])) + ",sep="") 
+                          col = names(data[[",s,"]][[",vm,"]])[",vd,"], group = ",vd,")) + ",sep="") 
         }
         
         z[D] <- paste("geom_ribbon(data = data.frame(fun(data[[",s,"]][[",vm,"]][[",D,"]])), 
-                                                   aes(x = axx, ymin = apply(fun(data[[",s,"]][[",vm,"]][[",D,"]]),1,min),
-                                                   ymax = apply(fun(data[[",s,"]][[",vm,"]][[",D,"]]),1,max)),
+                                                     aes(x = axx, ymin = apply(fun(data[[",s,"]][[",vm,"]][[",D,"]]),1,min),
+                                                   ymax = apply(fun(data[[",s,"]][[",vm,"]][[",D,"]]),1,max),
+                                                                  group = ",D,"), 
                                                    fill = colorList[",D,"], alpha = 0.4) + 
                       
                       geom_line(data = data.frame(fun(data[[",s,"]][[",vm,"]][[",D,"]])),
                       colour = colorList[",D,"],
                       aes(x = axx, y = apply(fun(data[[",s,"]][[",vm,"]][[",D,"]]),1,median), 
-                      col = names(data[[",s,"]][[",vm,"]])[",D,"]))",sep="")
+                      col = names(data[[",s,"]][[",vm,"]])[",D,"], group = ",D,"))",sep="")
         
         z <- paste(z, collapse = "")
         
