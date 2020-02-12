a8c7fd053392af35528f33b921b7b5e89304fe13
Sophie Castel <sophiecastel@trentu.ca>
2020-02-12 11:24:51 -0500

    Added function to re-stitch interpolated data given a directory containing the interpolated files from splitData.

---
+++ R/stitchData.R
@@ -0,0 +1,47 @@
+#' Stitch Data 
+#' 
+#' Function to stitch together data
+#' 
+#' @param directory The directory location of the .rda files to stitch. Must be devoid of other files and each file must be identical in structure. 
+
+
+stitchData <- function(directory){
+  
+  files <- list.files(directory, pattern=".rda", full.names = TRUE, recursive = TRUE)
+  split <- length(files)
+  
+  fileList <- list()
+  for(i in 1:split){
+  fileList[[i]] <- load_obj(files[i]) 
+  }
+  
+  names(fileList) <- letters[1:split]
+  
+  M <- length(fileList[[1]])
+  P <- length(fileList[[1]][[1]])
+  G <- length(fileList[[1]][[1]][[1]])
+  K <- length(fileList[[1]][[1]][[1]][[1]])*split
+    
+  stitchList <-  lapply(stitchList <- vector(mode = 'list',M), function(x)
+    lapply(stitchList <- vector(mode = 'list', P), function(x) 
+        x<-vector(mode='list',G)))
+  
+  names(stitchList) <- names(fileList[[1]])
+  names(stitchList[[1]]) <- names(fileList[[1]][[1]])
+  names(stitchList[[1]][[1]]) <- names(fileList[[1]][[1]][[1]])
+
+    for(m in 1:M){
+      for(p in 1:P){
+        for(g in 1:G){
+          for(i in 1:split){
+          stitchList[[m]][[p]][[g]] <- append(stitchList[[m]][[p]][[g]],fileList[[i]][[m]][[p]][[g]])
+          }
+          names(stitchList[[m]][[p]]) <- names(fileList[[1]][[1]][[1]])
+      }
+    }
+  }
+
+  return(stitchList)
+  }
+
+
