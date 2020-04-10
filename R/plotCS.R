3ec24dc59c7b962f6d22552dbc061c14dc8d790a
Sophie Castel <sophiecastel@trentu.ca>
2020-04-10 19:32:33 -0400

    better error messages and logical checks

--- R/plotCS.R
+++ R/plotCS.R
@@ -1,47 +1,68 @@
-#' Plot Cross Section
+#' Plot Collapsed Cross-sections by 'Gap Width' or 'Proportion Missing'
 #' 
-#' Function to generate cross-sectional plots to visualize changes in the performance metrics of interest
-#' as the proportion of missing data and gap width increase. \cr
-#' Metrics are aggregated across K simulations in each p,g,d,m specification, and plotted with respect to 
-#' a particular variable (p or g), collapsing across the other.  \cr
-#' Middle line is the median with respect to \code{f}, upper
-#' and lower ribbon limits are \code{min} and \code{max}, respectively. \cr
-#' The x-axis represents \code{p} or \code{g}, the proportion of missing data, or gap width.\cr 
-#' The y-axis represents the value of the performance metric of interest, collapsed (median) across the `other variable'.
-#' The user decides which variable to show on the x-axis.
+#' Function to generate collapsed cross-section plots to visualize changes in the performance metrics of interest
+#' as the \strong{proportion of missing data} and \strong{gap width increase}. Instead of three axes, as in the surface plots, 
+#' either \code{p} or \code{g} can be 'collapsed' such that changes in performance are depicted with respect to one variable, 
+#' across all values of the other. This variable is specified in \code{cross_section}, and will form the x-axis of the cross-section plot.
+#' \itemize{
+#' \item The middle line is the \strong{median} value of the collapsed variable \cr
+#' \item The upper ribbon boundary is the \strong{maximum} value \cr
+#' \item The lower ribbon boundary is the \strong{minimum} value \cr
+#' }
 #' 
-#' @param d A vector of the indexes of the datasets of interest
-#' @param m character; A vector of the interpolation methods of interest (maximum of 5)
-#' @param crit A character vector describing the performance metrics of interest
-#' @param agEval A list object (result of agEval.R) of aggregated performance metrics
-#' @param layer_type "method" (default) or "dataset"; how to slice the data
-#' @param f "median" (default); which statistic to use for f(p,g)
-#' @param cross_section "p" or "g"; which axis to show against criterion value 
+#' @param d \code{numeric}; A vector to indicate datasets of interest
+#' @param m \code{character}; A vector of interpolation methods of interest
+#' @param crit \code{character}; An element describing the performance metric of interest
+#' @param agEval \code{agEvaluate}; An object containing the aggregated performance metrics (result of \code{agEvaluate()})
+#' @param layer_type \code{character}; How to layer the ribbons (by "method" or by "dataset") 
+#' @param f \code{character}; The statistic of interest defining the ribbon. Possible choices are listed in \code{?agEvaluate}
+#' @param cross_section \code{character}; An element describing the gap structure variable to represent on the x-axis: either \code{"p"} or \code{"g"}
+#' @param highlight \code{character/numeric}; A single method (if \code{layer_type = "method"}) or dataset (if \code{layer_type = "dataset"}) to highlight.
+#' @param highlight_color \code{character}; An HTML color of format \code{"#xxxxxx"} to apply to \code{highlight}
+#' @param colors \code{character}; A vector of the desired color palette, with entries in HTML format (\code{"#xxxxxx"}) 
 
-plotCS <- function(d=1:length(agEval), 
-                        m=names(agEval[[1]][[1]][[1]]), 
-                        crit, 
-                        agEval, 
-                        layer_type = "method", 
-                        f = "median", 
-                        cross_section = "p",
-                        highlight = "HWI", highlight_color = "#EE5C42",
-                        colors = c("#F9E0AA","#F7C65B","#FAAF08","#FA812F","#FA4032","#F92111"),
-                        showLegend = T){
+plotCS <- function(d = 1:length(agEval), 
+                   m=names(agEval[[1]][[1]][[1]]), 
+                   crit, 
+                   agEval, 
+                   layer_type = "method", 
+                   f = "median", 
+                   cross_section = "p",
+                   highlight = "HWI", highlight_color = "#EE5C42",
+                   colors = c("#F9E0AA","#F7C65B","#FAAF08","#FA812F","#FA4032","#F92111")){
   
-  require(plotly)
-  require(dplyr)
-  require(RColorBrewer)
+  ## LOGICAL CHECKS ############
   
-  stopifnot((layer_type == "method" || layer_type == "dataset"),
-            f %in% names(agEval[[1]][[1]][[1]][[1]])[1:11],
-            (cross_section == "p" || cross_section == "g"),
-            class(agEval) == "agEvaluate",
-            length(d) <= length(agEval),
-            length(m) <= length(agEval[[1]][[1]][[1]]),
-            crit %in% rownames(agEval[[1]][[1]][[1]][[1]])
-  )
+  if(sum(duplicated(d) != 0)) stop(paste0("'d' contains redundant elements at position(s): ", paste0(c(1:length(d))[duplicated(d)], collapse = ", ") ))
+  if(sum(duplicated(m) != 0)) stop(paste0("'m' contains redundant elements at position(s): ", paste0(c(1:length(m))[duplicated(m)], collapse = ", ") ))
   
+  if(layer_type != "method" & layer_type != "dataset") stop("'layer_type' must equal either 'method' or 'dataset'.")
+  if(cross_section != "p" & cross_section != "g") stop("'cross_section' must equal either 'p' or 'g'.")
+  
+  if(!all(f %in% names(agEval[[1]][[1]][[1]][[1]])[1:12])) stop(paste0(c("f must be one of: '",paste0(names(agEval[[1]][[1]][[1]][[1]])[1:12], collapse = "', '"),"'."), collapse = ""))
+  if(!all(paste0("D",d) %in% names(agEval))) stop("Dataset(s) ", paste0(d[!paste0("D",d) %in% names(agEval)], collapse = ", ")," not found. Possible choices are: ", paste0(gsub("D", "",names(agEval)), collapse = ", "))
+  if(!all(m %in%  names(agEval[[1]][[1]][[1]]))) stop("Method(s) '", paste0(m[!m %in% names(agEval[[1]][[1]][[1]])], collapse = ", ' "),"' not found. Possible choices are: '", paste0(names(agEval[[1]][[1]][[1]]), collapse = "', '"),"'.")
+  if(!crit %in% rownames(agEval[[1]][[1]][[1]][[1]])) stop(paste0("Criterion '",crit,"' must be one of ", paste(rownames(agEval[[1]][[1]][[1]][[1]]),collapse = ", "),"."))
+  
+  if(class(agEval) != "agEvaluate") stop("'agEval' object must be of class 'agEvaluate'. Please use agEvaluate().")
+  
+  if(layer_type == "method" & !highlight %in% m) stop(paste0(c("'highlight' must be an element of 'm'. Choose one of: '", paste0(m, collapse = "', '"),"'."), collapse = ""))
+  if(layer_type == "dataset" & !highlight %in% d) stop(paste0(c("'highlight' must be an element of 'd'. Choose one of: '", paste0(d, collapse = "', '"),"'."), collapse = ""))
+  
+  if(length(crit) != 1) stop("'crit' must contain only a single character element.")
+  if(length(f) != 1) stop("'f' must contain only a single character element.")
+  if(length(layer_type) != 1) stop("'layer_type' must contain only a single character element.")
+  if(length(cross_section) != 1) stop("'cross_section' must contain only a single character element.")
+  if(length(highlight) != 1) stop("'highlight' must contain only a single character element.")
+  if(length(highlight_color) != 1) stop("'highlight_color' must contain only a single character element.")
+  
+  if(length(colors) < 2) stop("'colors' must contain at least two colors (each in HTML format: '#xxxxxx')")
+  
+  if(layer_type == "dataset" & !is.numeric(highlight)) stop("If 'layer_type' = 'dataset', then 'highlight' must be of class 'numeric'.")
+  if(layer_type == "method" & !is.character(highlight)) stop("If 'layer_type' = 'method', then 'highlight' must be of class 'character'.")
+  
+  ##################
+
   P <- length(agEval[[1]])
   G <- length(agEval[[1]][[1]])
   prop_vec_names <- names(agEval[[1]])
