983a7f64e4d1e5d84a61cbb4a154b38b6ace93ed
Sophie Castel <sophiecastel@trentu.ca>
2020-04-10 19:23:17 -0400

    Sped up aggregation by removing redundant quantile() calculations. Fixed method labeling bug.

--- R/agEvaluate.R
+++ R/agEvaluate.R
@@ -1,11 +1,30 @@
 #' Aggregate the Performance Matrices of Multiple Interpolations
 #' 
-#' Function to 'condense' the performance matrices of the interpolations from each method (M) across K simulations in each (P,G) missingness specification 
-#' @param pmats The performance matrices (result of performance.R)
-#' @param hist logical. TRUE returns a list of histograms of criteria, FALSE returns matrix of aggregated metrics
+#' Function to aggregate the set of performance matrices, by criterion, using sample statistics of the sampling distribution across K. Resulting object is of class \code{'agEvaluate'}.
+#'  
+#' @param pmats \code{list}; A nested list of dimension D x M x P x G x K (result of \code{performance.R}), where the terminal node is a performance matrix.
+#' 
+#' @details The statistics provided in the output are as follows:
+#' \itemize{
+#' \item mean
+#' \item standard deviation 
+#' \item minimum (0\% quantile)
+#' \item 2.5\% quantile
+#' \item 25\% quantile
+#' \item median (50\% quantile)
+#' \item 75\% quantile
+#' \item 97.5\% quantile
+#' \item maximum (100\% quantile)
+#' \item IQR (97.5\% quantile - 2.5\% quantile)
+#' \item skewness
+#' \item p-value of dip test for unimodality (see \code{?dip} for details)
+#' }
 
-agEvaluate <- function(pmats, hist = F){
-  skew <- function(x, na.rm = TRUE){
+agEvaluate <- function(pmats){
+  
+  if(class(pmats) != "pmat") stop("'pmats' object must be of class 'pmat'. Use performance() to generate such objects.")
+
+    skew <- function(x, na.rm = TRUE){
     stopifnot(is.numeric(x))
     
     if(na.rm){
@@ -38,7 +57,7 @@ agEvaluate <- function(pmats, hist = F){
   
   prop_vec_names <- numeric(P)
   gap_vec_names <- numeric(G)
-  method_names <- numeric(M)
+  method_names <- character(M)
   
   prop_vec <- as.numeric(gsub("p","",names(pmats[[1]][[1]])))
   gap_vec <- as.numeric(gsub("g","",names(pmats[[1]][[1]][[1]])))
@@ -51,51 +70,56 @@ agEvaluate <- function(pmats, hist = F){
         gap_vec_names[g] <- c(paste("g", gap_vec[g],sep="")) # vector of names
         for(m in 1:M){
           method_names[m] <- names(pmats[[1]])[m]
-
-          # Generate histograms in each subset
-          if(hist){
-            sqrtC = sqrt(C)
-            par(mfrow=c(floor(sqrtC),ceiling(sqrtC)))
-            Evaluation[[d]][[p]][[g]][[m]] <- apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,hist)
-          }
           
             # compute the mean and distribution of the performance criteria in each (d,m,p,g) specification across all k pairs of (x,X) and 
             # store results in a list of data frames
-          else if(!hist){
+          
+            quantiles <- apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1, 
+                               FUN=function(x) quantile(x, probs = c(0, 0.025, 0.25, 0.75, 0.975, 1.0), na.rm = TRUE))
+            
             Evaluation[[d]][[p]][[g]][[m]] <- data.frame(
               
               mean = rowMeans(sapply(pmats[[d]][[m]][[p]][[g]],unlist), na.rm = TRUE),
               
               sd = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,sd, na.rm = TRUE),
               
-              q0 = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,quantile, na.rm = TRUE)["0%",],
+              #q0 = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,quantile, na.rm = TRUE)["0%",],
+              q0 = quantiles["0%",],
               
-              q2.5 = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1, 
-                           FUN=function(x) quantile(x, probs = c(0.025,0.975), na.rm = TRUE))["2.5%",],
+              #q2.5 = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1, 
+                           #FUN=function(x) quantile(x, probs = c(0.025,0.975), na.rm = TRUE))["2.5%",],
+              q2.5 = quantiles["2.5%",],
               
-              q25 = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,quantile, na.rm = TRUE)["25%",],
+              #q25 = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,quantile, na.rm = TRUE)["25%",],
+              q25 = quantiles["25%",],
               
               median = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,median, na.rm = TRUE),
               
-              q75 = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,quantile, na.rm = TRUE)["75%",],
+              #q75 = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,quantile, na.rm = TRUE)["75%",],
+              q75 = quantiles["75%",],
               
-              q97.5 = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1, 
-                            FUN=function(x) quantile(x, probs = c(0.025,0.975), na.rm = TRUE))["97.5%",],
+              #q97.5 = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1, 
+                            #FUN=function(x) quantile(x, probs = c(0.025,0.975), na.rm = TRUE))["97.5%",],
+              q97.5 = quantiles["97.5%",],
               
-              q100 = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,quantile, na.rm = TRUE)["100%",],
+              #q100 = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,quantile, na.rm = TRUE)["100%",],
+              q100 = quantiles["100%",],
+              
+              iqr = quantiles["97.5%",] - quantiles["2.5%",],
               
               skewness = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,skew), 
               
               dip = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,
                           FUN = function(x){dip.test(x,simulate.p.value = TRUE)$p.value
+                            
               }),
               
               gap_width = c(rep(gap_vec[g], C)),
               prop_missing = c(rep(prop_vec[p],C)),
               dataset = c(rep(dataset[d],C)), 
-              method = rep(names(pmats[[1]])[m],C) 
+              method = rep(method_names[m],C) 
             )  
-          }
+          
             
         }
         names(Evaluation[[d]][[p]][[g]]) <- method_names 
