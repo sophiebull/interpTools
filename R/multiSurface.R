eba4fe3a1fc53ae02b242606093291d969cd7d88
Sophie Castel <sophiecastel@trentu.ca>
2020-04-10 19:31:41 -0400

    better error messages and logical checks

--- R/multiSurface.R
+++ R/multiSurface.R
@@ -1,7 +1,27 @@
-multiSurface <- function(agEval, 
-                         d=1:length(agEval),
-                         m=names(agEval[[1]][[1]][[1]]),
+#' Arrange Multiple Surface Plots on a Grid
+#' 
+#' Function to generate a grid of surface plots (from the \code{plotly} package) to visualize changes in the performance metrics of interest
+#' with gap structure, across a number of datasets or methods. The structure of each surface plot is described in \code{?plotSurface}. \cr \cr
+#' \itemize{
+#' \item If \code{layer_type = "method"}, plots are arranged in a grid, by \strong{dataset}. \cr
+#' \item If \code{layer_type = "dataset"}, plots will be arranged in a grid, by \strong{method}. \cr
+#' }
+#' 
+#' @param d \code{numeric}; A vector to indicate datasets of interest
+#' @param m \code{character}; A vector of interpolation methods of interest
+#' @param crit \code{character}; An element describing the performance metric of interest
+#' @param agEval \code{agEvaluate}; An object containing the aggregated performance metrics (result of \code{agEvaluate()})
+#' @param layer_type \code{character}; How to layer the surfaces (by "method" or by "dataset") 
+#' @param f \code{character}; The statistic of interest defining the surface \code{f(p,g)}. Possible choices are listed in \code{?agEvaluate}.
+#' @param highlight \code{character/numeric}; A single method (if \code{layer_type = "method"}) or dataset (if \code{layer_type = "dataset"}) to highlight.
+#' @param highlight_color \code{character}; An HTML color of format \code{"#xxxxxx"} to apply to \code{highlight}
+#' @param colors \code{character}; A vector of the desired color palette, with entries in HTML format (\code{"#xxxxxx"}) 
+#' 
+
+multiSurface <- function(d = 1:length(agEval),
+                         m = names(agEval[[1]][[1]][[1]]),
                          crit,
+                         agEval, 
                          layer_type = "method",
                          f = "median",
                          highlight = "HWI",
@@ -9,10 +29,44 @@ multiSurface <- function(agEval,
                          colors = c("#F9E0AA","#F7C65B","#FAAF08","#FA812F","#FA4032","#F92111")
                          ){
  
-  stopifnot(length(crit) == 1)
+  ## LOGICAL CHECKS ############
+  
+  if(sum(duplicated(d) != 0)) stop(paste0("'d' contains redundant elements at position(s): ", paste0(c(1:length(d))[duplicated(d)], collapse = ", ") ))
+  if(sum(duplicated(m) != 0)) stop(paste0("'m' contains redundant elements at position(s): ", paste0(c(1:length(m))[duplicated(m)], collapse = ", ") ))
+  
+  if(!all(m %in%  names(agEval[[1]][[1]][[1]]))) stop("Method(s) '", paste0(m[!m %in% names(agEval[[1]][[1]][[1]])], collapse = ", ' "),"' not found. Possible choices are: '", paste0(names(agEval[[1]][[1]][[1]]), collapse = "', '"),"'.")
+  if(!all(paste0("D",d) %in% names(agEval))) stop("Dataset(s) ", paste0(d[!paste0("D",d) %in% names(agEval)], collapse = ", ")," not found. Possible choices are: ", paste0(gsub("D", "",names(agEval)), collapse = ", "))
+  if(!all(f %in% names(agEval[[1]][[1]][[1]][[1]])[1:12])) stop(paste0(c("f must be one of: '",paste0(names(agEval[[1]][[1]][[1]][[1]])[1:12], collapse = "', '"),"'."), collapse = ""))
+  if(!crit %in% rownames(agEval[[1]][[1]][[1]][[1]])) stop(paste0("Criterion '",crit,"' must be one of ", paste(rownames(agEval[[1]][[1]][[1]][[1]]),collapse = ", "),"."))
+  
+  if(length(crit) != 1) stop("'crit' must contain only a single character element.")
+  if(length(f) != 1) stop("'f' must contain only a single character element.")
+  if(length(layer_type) != 1) stop("'layer_type' must contain only a single character element.")
+  if(length(highlight) != 1) stop("'highlight' must contain only a single character element.")
+  if(length(highlight_color) != 1) stop("'highlight_color' must contain only a single character element.")
+  
+  if(layer_type != "method" & layer_type != "dataset") stop("'layer_type' must equal either 'method' or 'dataset'.")
+  
+  if(class(agEval) != "agEvaluate") stop("'agEval' object must be of class 'agEvaluate'. Please use agEvaluate().")
+  
+  if(layer_type == "method" & !highlight %in% m) stop(paste0(c("'highlight' must be an element of 'm'. Choose one of: '", paste0(m, collapse = "', '"),"'."), collapse = ""))
+  if(layer_type == "dataset" & !highlight %in% d) stop(paste0(c("'highlight' must be an element of 'd'. Choose one of: '", paste0(d, collapse = "', '"),"'."), collapse = ""))
+  
+  if(length(colors) <2) stop("'colors' must contain at least two colors (each in HTML format: '#xxxxxx')")
+  
+  if(layer_type == "dataset" & !is.numeric(highlight)) stop("If 'layer_type' = 'dataset', then 'highlight' must be of class 'numeric'.")
+  if(layer_type == "method" & !is.character(highlight)) stop("If 'layer_type' = 'method', then 'highlight' must be of class 'character'.")
+  
+  if(layer_type == "method" & length(d) < 2) stop("Only 1 dataset was chosen. Please specify at least one more, or use plotSurface() instead.") 
+  if(layer_type == "dataset" & length(m) < 2) stop("Only 1 method was chosen. Please specify at least one more, or use plotSurface() instead.") 
+  
+  if(layer_type == "method" & length(d) < 3){ warning("multiSurface() layout works best with at least three plots. Next time, specify at least 3 elements in 'd'.")}
+  if(layer_type == "dataset" & length(m) < 3){ warning("multiSurface() layout works best with at least three plots. Next time, specify at least 3 elements in 'm'.")}
   
   D <- length(d)
   M <- length(m)
+  
+  ##################
   
   # BUILD LEGEND ###########################################################################
   z_list <- compileMatrix(agEval=agEval)[[f]]
