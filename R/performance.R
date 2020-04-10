df0e7db27e463e3795d55638bbd15f0fb8c618d3
Sophie Castel <sophiecastel@trentu.ca>
2020-04-10 19:26:54 -0400

    Object is now of new class "pmat".

--- R/performance.R
+++ R/performance.R
@@ -1,10 +1,14 @@
 #' Evaluate Interpolation Performance Across Multiple Time Series
 #' 
-#' Function to calculate the performance metrics between lists of original and interpolated series.
-#' @param OriginalData A list object of dimension Dxn of original (complete) time series 
-#' @param IntData A list object of dimension DxMxPxGxKxN of interpolated time series (output of parInterpolate.R)
-#' @param GappyData A list object of dimension DxPxGxKxN of the gappy original time series (output of simulateGaps.R)
+#' Function to calculate the performance metrics between lists of original and interpolated series. \cr \cr
+#' See \code{?eval_performance} and additional documentation provided in this package (\code{"~/metric_definitions.pdf/"}) for a full list and description of the the performance criteria.
 #' 
+#' @param OriginalData \code{list}; A list object of dimension D x N of original (complete) time series 
+#' @param IntData \code{list}; A list object of dimension D x M x P x G x K x N of interpolated time series (output of parInterpolate.R)
+#' @param GappyData \code{list}; A list object of dimension D x P x G x K x N of the gappy original time series (output of simulateGaps.R)
+#' 
+#' 
+
 performance <- function(OriginalData,IntData,GappyData){
   
   D <- length(IntData)
@@ -26,11 +30,11 @@ performance <- function(OriginalData,IntData,GappyData){
   
   prop_vec <- as.numeric(gsub("p","",names(IntData[[1]][[1]])))
   gap_vec <- as.numeric(gsub("g","",names(IntData[[1]][[1]][[1]])))
+  method_names <- names(IntData[[1]])
     
   # Evaluate the performance criteria for each sample in each (d,m,p,g) specification
   for(d in 1:D){
     for(m in 1:M){
-      method_names[m] <- names(IntData[[1]])[m] 
       for(p in 1:P){
         prop_vec_names[p] <- c(paste("p", prop_vec[p],sep="")) # vector of names
         for(g in 1:G){
@@ -55,5 +59,6 @@ performance <- function(OriginalData,IntData,GappyData){
                             x <-x[logic]
                       }))))
   
+  class(Performance) <- "pmat"
   return(Performance) 
 }
