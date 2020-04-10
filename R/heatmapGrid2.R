d6d6bde7e8551e29b22fe1115f462d904f3d3f1e
Sophie Castel <sophiecastel@trentu.ca>
2020-04-10 19:33:27 -0400

    better error messages and logical checks, fixed titling bug. Psi now reflects the exact user input to d

--- R/heatmapGrid2.R
+++ R/heatmapGrid2.R
@@ -1,5 +1,45 @@
-heatmapGrid2 <- function(agEval, f="median", crit, m, colors = c("#F9E0AA","#F7C65B","#FAAF08","#FA812F","#FA4032","#F92111"), d = 1:5){
+#'  Generate Heatmaps of Performance
+#' 
+#'  A function to generate a set of heatmaps depicting the performances of particular sets of interpolations. This graphic can be considered as a series of 'compressed' surface plots, arranged by dataset.\cr
+#'  
+#' @param agEval \code{agEvaluate}; An object containing the aggregated performance metrics (result of \code{agEvaluate()})
+#' @param f \code{character}; The statistic of interest that will be depicted by the heatmap. Possible choices are listed in \code{?agEvaluate}.
+#' @param crit \code{character}; A single element describing the performance metric of interest
+#' @param m \code{character}; A single element describing the interpolation methods of interest
+#' @param d \code{numeric}; A vector to indicate datasets of interest
+#' @param colors \code{character}; A vector of the desired color palette, with entries in HTML format (\code{"#xxxxxx"}) 
 
+heatmapGrid2 <- function(agEval, 
+                         f="median", 
+                         crit, 
+                         m, 
+                         d = 1:length(agEval), 
+                         colors = c("#F9E0AA","#F7C65B","#FAAF08","#FA812F","#FA4032","#F92111")){
+
+  ## LOGICAL CHECKS ############
+  
+  if(sum(duplicated(d) != 0)) stop(paste0("'d' contains redundant elements at position(s): ", paste0(c(1:length(d))[duplicated(d)], collapse = ", ") ))
+  if(sum(duplicated(m) != 0)) stop(paste0("'m' contains redundant elements at position(s): ", paste0(c(1:length(m))[duplicated(m)], collapse = ", ") ))
+  if(sum(duplicated(crit) != 0)) stop(paste0("'crit' contains redundant elements at position(s): ", paste0(c(1:length(crit))[duplicated(crit)], collapse = ", ") ))
+  
+  if(by != "crit" & by != "method") stop("'by' must be either 'crit' or 'method'.")
+  
+  if(class(agEval) != "agEvaluate") stop("'agEval' object must be of class 'agEvaluate'. Please use agEvaluate().")
+  
+  if(length(crit) != 1) stop("'crit' must contain only a single character element.")
+  if(length(f) != 1) stop("'f' must contain only a single character element.")
+  if(length(m) != 1) stop("'m' must contain only a single character element.")
+  if(length(by) != 1) stop("'by' must contain only a single character element.")
+  
+  if(!(m %in%  names(agEval[[1]][[1]][[1]]))) stop(paste0("Method '", m,"' not found. Possible choices are: '", paste0(names(agEval[[1]][[1]][[1]]), collapse = "', '"),"'."))
+  if(!all(paste0("D",d) %in% names(agEval))) stop("Dataset(s) ", paste0(d[!paste0("D",d) %in% names(agEval)], collapse = ", ")," not found. Possible choices are: ", paste0(gsub("D", "",names(agEval)), collapse = ", "))
+  if(!all(f %in% names(agEval[[1]][[1]][[1]][[1]])[1:12])) stop(paste0(c("f must be one of: '",paste0(names(agEval[[1]][[1]][[1]][[1]])[1:12], collapse = "', '"),"'."), collapse = ""))
+  if(!crit %in% rownames(agEval[[1]][[1]][[1]][[1]])) stop(paste0("Criterion '",crit,"' must be one of ", paste(rownames(agEval[[1]][[1]][[1]][[1]]),collapse = ", "),"."))
+  
+  if(length(colors) <2) stop("'colors' must contain at least two colors (each in HTML format: '#xxxxxx')")
+  
+ ##################
+  
   # get legend
   g_legend<-function(a.gplot){
     tmp <- ggplot_gtable(ggplot_build(a.gplot))
@@ -20,7 +60,7 @@ heatmapGrid2 <- function(agEval, f="median", crit, m, colors = c("#F9E0AA","#F7C
     plott[[vd]] <- melt(z_list[[crit]][[m]][[d[vd]]])
     colnames(plott[[vd]]) <- c("p","g", "value")
     
-    titles[vd] <- paste0("bquote(psi ==",vd*10,")")
+    titles[vd] <- paste0("bquote(psi ==",d[vd]*10,")")
     
   }
   
