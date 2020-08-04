4c88ed04b1986a6eafd3019c8f6f9b56ad14a5a9
Sophie Castel <sophiecastel@trentu.ca>
2020-08-04 15:43:19 -0400

    n (deprecated) ----> N

--- R/simMt.R
+++ R/simMt.R
@@ -1,40 +1,40 @@
 #' Simulate M_t
 #' 
 #' Function to simulate M_t: the mean component of X_t. 
-#' @param n The length of the output series
+#' @param N The length of the output series
 #' @param numTrend The number of terms to include in the varying trend component of M_t. Value represents the maximum degree; all lower order terms included.
 #' @param trendType The type of trends to include. Either "polynomial" or "exponential".
  
-simMt <- function(n = 1000, numTrend = 0, trendType = "polynomial"){
+simMt <- function(N = 1000, numTrend = 0, trendType = "polynomial"){
   
   if(trendType != "polynomial" && trendType != "exponential"){
     warning("Please specify trendType as either 'polynomial' or 'exponential'.")
     stop()
   }
   
-  stopifnot(is.numeric(n), n>=10, is.numeric(numTrend), numTrend>=0, (trendType == "polynomial" | trendType == "exponential"))
+  stopifnot(is.numeric(N), N>=10, is.numeric(numTrend), numTrend>=0, (trendType == "polynomial" | trendType == "exponential"))
   
   
   Mt_list <- list()
-  t <- 0:(n-1)
+  t <- 0:(N-1)
   
   ######### Initializing Mt 
-  mu <- runif(1, -n/100, n/100)
+  mu <- runif(1, -N/100, N/100)
   mut <- numeric(numTrend)
   
   if(trendType == "polynomial"){
     
     if(numTrend > 0){
-      center <- sample(1:n, 1)
+      center <- sample(1:N, 1)
       if(numTrend > 1){
         for(k in 1:(numTrend-1)){
-          a <- rnorm(1, mean = 0, sd = n/(20*k))
-          mut[k] <- paste("(",a,")*((t-center)/n)^",k,"+",sep="")
+          a <- rnorm(1, mean = 0, sd = N/(20*k))
+          mut[k] <- paste("(",a,")*((t-center)/N)^",k,"+",sep="")
         }
       }
       
-      a <- rnorm(1, mean = 0, sd = n/(20))
-      mut[numTrend] <- paste("(",a,")*((t-center)/n)^",numTrend,sep="")
+      a <- rnorm(1, mean = 0, sd = N/(20))
+      mut[numTrend] <- paste("(",a,")*((t-center)/N)^",numTrend,sep="")
       Mt_0 = NULL
     }
     
@@ -47,13 +47,13 @@ simMt <- function(n = 1000, numTrend = 0, trendType = "polynomial"){
     if(numTrend > 0){
       if(numTrend > 1){
         for(k in 1:(numTrend-1)){
-          a <- sample(-(n/10):(n/10),1)
-          mut[k] <- paste("(",a,")*exp(",k,"*(t/n))+",sep="")
+          a <- sample(-(N/10):(N/10),1)
+          mut[k] <- paste("(",a,")*exp(",k,"*(t/N))+",sep="")
         }
       }
       
-      a <- sample(-(n/10):(n/10),1)
-      mut[numTrend] <- paste("(",a,")*exp(",numTrend,"*(t/n))",sep="")
+      a <- sample(-(N/10):(N/10),1)
+      mut[numTrend] <- paste("(",a,")*exp(",numTrend,"*(t/N))",sep="")
       Mt_0 = NULL
     }
     
@@ -69,7 +69,7 @@ simMt <- function(n = 1000, numTrend = 0, trendType = "polynomial"){
   Mt <- mut+mu
   
   if(numTrend == 0){
-    Mt <- rep(mu,n)
+    Mt <- rep(mu,N)
   }
   
   Mt_list$fn <- Mt_fn
