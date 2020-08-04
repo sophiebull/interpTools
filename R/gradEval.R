7d6047f4c82ca384fd3e67036257578e94e569bf
Sophie Castel <sophiecastel@trentu.ca>
2020-08-04 15:46:54 -0400

    Fixed reporting to be more descriptive.  Fixed issue with sorting and resulting report having non-conforming ratios
    More streamlined methods of building lists

--- R/gradEval.R
+++ R/gradEval.R
@@ -1,14 +1,25 @@
-gradEval <- function(agEval, crit, m = names(agEval[[1]][[1]][[1]]), d = 1:length(names(agEval)), cm, cd, f = "median"){
+gradEval <- function(agEval, crit, m = names(agEval[[1]][[1]][[1]]), d = 1:length(agEval), cm, cd = 1, f = "median"){
   
-  stopifnot(cm %in% m)
+  if(!(cm %in% m)) stop(paste0("Chosen method '", cm, "' must be one of ", paste0(m, collapse = ", ") ,"." ))
+
+  criteria <- rownames(agEval[[1]][[1]][[1]][[1]])
+  maximize <- c(1,1,rep(0,11),1,rep(0,4)) # 1 = yes, 0 = no
+  optimal <- maximize
+  optimal[which(optimal == "1")] <- "max"
+  optimal[which(optimal == "0")] <- "min"
+  
+  best <- data.frame(criteria = criteria, 
+                     maximize = maximize,
+                     optimal = optimal)
+  
   
   gradList <- gradient(agEval = agEval, crit = crit, f = f, d = d, m = m)
 
-  om = m[!(m %in% cm)]
+  om = m[!(m %in% cm)] # other method
   
-  cmGrad <- gradList[[crit]][[cm]][[cd]]
+  cmGrad <- gradList[[crit]][[cm]][[cd]] # chosen gradient
   
-  omList <- list()
+  omList <- list() # other gradients
   
   for(vm in 1:length(om)){
     omList[[vm]] <- eval(parse(text = paste0("gradList[[crit]][[om[",vm,"]]][[cd]]"))) 
@@ -22,30 +33,47 @@ gradEval <- function(agEval, crit, m = names(agEval[[1]][[1]][[1]]), d = 1:lengt
   gradIndex <- which.max(cmGrad[,"slope"])
 
   #Compare to other methods at that point
-  string <- paste0(om[1:(length(om)-1)]," = omList[[",1:(length(om)-1),"]][gradIndex,],",  collapse = "")
-  string <- c("list(",paste0(cm," = cmGrad[gradIndex,],"),string, paste0(om[length(om)]," = omList[[",length(om),"]][gradIndex,]"),")")
-
-  comList <- eval(parse(text = string))
   
-  string <- paste0(om[1:(length(om)-1)]," = comList[[",2:(length(om)),"]]['slope']/comList[[1]]['slope'],")
-  string <- c("list(",string,paste0(om[length(om)]," = comList[[",length(om)+1,"]]['slope']/comList[[1]]['slope']"),")")
+  string <- paste0(cm, "= cmGrad[gradIndex,]")
+  string <- c("list(", paste0(om," = omList[['",names(omList),"']][gradIndex,]", sep = ","), string,")")
+  
+  comList <- eval(parse(text = string)) # combined gradient list (chosen and other)
+  
+  string <- c("list(", paste0(om, " = comList[['",om,"']]['slope']/comList[['", cm,"']]['slope']", sep = ","))
+  string <- paste(string, collapse = "")
+  string <- c(substr(string, 1, nchar(string)-1),")")
   
   divs<- eval(parse(text = string))  
-  divs <- sort(unlist(divs), decreasing = FALSE)
+  divs <- lapply(divs, FUN = abs)
   
-  om <- gsub(".slope","",names(divs))
+  #om <- gsub(".slope","",names(divs))
   
   #Statements
   
-  string1 <- paste0("Region of max instability (",cm,", dataset ", cd,", ", crit,") : (p1 = ",cmGrad[gradIndex,'p1'],", g1 = ", cmGrad[gradIndex,'g1'],") to (p2 = ", cmGrad[gradIndex,'p2'],", g2 = ", cmGrad[gradIndex,'g2'],")")
-  
-  reportList <- list(string1)
+
+  cat(paste0("Method under inspection: '", cm, "'"),
+      paste0("Region of max instability:(p1 = ",cmGrad[gradIndex,'p1'],", g1 = ", cmGrad[gradIndex,'g1'],") to (p2 = ", cmGrad[gradIndex,'p2'],", g2 = ", cmGrad[gradIndex,'g2'],")"),
+      paste0("Corresponding slope: ", round(comList[[cm]]['slope'], 2)),
+      paste0(" "),
+      paste0("REPORT:"),sep = "\n")
   
   for(vm in 1:length(om)){
-    reportList[[vm+1]] <- paste0("The ", om[vm], " is ", round(divs[[vm]],2)," times less stable.")
+    
+    if(abs(divs[[vm]]) < 1){
+      cat(paste0("slope of ", om[vm], ": ", comList[[om[vm]]]['slope']),
+          paste0("The '", om[vm], "' algorithm has ", round(divs[[vm]],2)," times the slope of '",cm,"': ", cm, " is ", 100 - round(divs[[vm]],2)*100,"% more stable"),
+          paste0(" "),
+          sep = '\n')
+    }
+    
+    else if(abs(divs[[vm]]) > 1 ){
+      cat(paste0("slope of ", om[vm], ": ", comList[[om[vm]]]['slope']),
+          paste0("The '", om[vm], "' algorithm has ", round(divs[[vm]],2)," times the slope of '",cm,"': ", om[vm], " is ", abs(100 - round(divs[[vm]],2)*100),"% more unstable"),
+          paste0(" "),
+          sep = '\n')
+    }
   }
   
-  return(reportList)
 }
 
 
