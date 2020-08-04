0997449f18e63b17a0e3cd76f7ea217c3a50fe22
Sophie Castel <sophiecastel@trentu.ca>
2020-08-04 15:43:12 -0400

    n (deprecated) ----> N

--- R/simTt.R
+++ R/simTt.R
@@ -1,24 +1,24 @@
 #' Simulate T_t
 #' 
 #' Function to simulate T_t: the trend component of X_t.
-#' @param n The length of the output series
+#' @param N The length of the output series
 #' @param bandwidth Value is plugged into a negative exponent (base 10) and defines some interval in which to sample frequencies. If specified, 'bandwidth' must be at least -log10(1/numFreq). If unspecified, freq =  runif(numFreq,0,1)
 #' @param numFreq The number of sinusoids to generate
 
-simTt <- function(n=1000, numFreq = 20, bandwidth = NULL){  
+simTt <- function(N=1000, numFreq = 20, bandwidth = NULL){  
   
   if( !is.numeric(numFreq)) stop('numFreq must be numeric.')
   if((!is.numeric(bandwidth) && !is.null(bandwidth))) stop('bandwidth must be NULL or numeric.')
   if( any(numFreq < 0 | numFreq%%2 != 0 | numFreq%%1 != 0) ) stop('Please specify an even integer for numFreq')
   if( !(is.null(bandwidth)) &&  bandwidth>=1 && numFreq > 1/(10^-bandwidth) ) stop('Bandwidth must be at least -log10(1/numFreq)')
-  if( !(is.numeric(n)) || n%%1 != 0) stop ('n must be a positive integer')
+  if( !(is.numeric(N)) || N%%1 != 0) stop ('N must be a positive integer')
   
-  stopifnot(n%%1 == 0, n >= 10, numFreq%%2 == 0, numFreq > 0, (is.null(bandwidth) || (bandwidth >= 1 && (numFreq <= 1/(10^-bandwidth)))),
+  stopifnot(N%%1 == 0, N >= 10, numFreq%%2 == 0, numFreq > 0, (is.null(bandwidth) || (bandwidth >= 1 && (numFreq <= 1/(10^-bandwidth)))),
             (is.null(bandwidth) | is.numeric(bandwidth)))
   
   Tt_list <- list()
-  t <- 0:(n-1)
-  fourierFreq <- 2*pi/n
+  t <- 0:(N-1)
+  fourierFreq <- 2*pi/N
   
   if(!is.null(bandwidth)){ # specified
   # choose random midpoints
@@ -50,7 +50,7 @@ simTt <- function(n=1000, numFreq = 20, bandwidth = NULL){
   
   #if(!ampMod){
     for(f in 1:(length(w)-1)){
-      a <- rnorm(1, mean = 0, sd = n/200) #95% runs from -n/100 to n/100
+      a <- rnorm(1, mean = 0, sd = N/200) #95% runs from -N/100 to N/100
       Tt[f] <- paste("(",a,")*sin(",w[f],"*t)+",sep="")
     }
     Tt[length(w)] <- paste("(",a,")*sin(",w[length(w)],"*t)",sep="")
