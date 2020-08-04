f8a4290a079df4ae077c912c436d4ad964ab0af0
Sophie Castel <sophiecastel@trentu.ca>
2020-08-04 15:47:32 -0400

    pmats (deprecated) --- > pf

--- R/agEvaluate.R
+++ R/agEvaluate.R
@@ -2,7 +2,7 @@
 #' 
 #' Function to aggregate the set of performance matrices, by criterion, using sample statistics of the sampling distribution across K. Resulting object is of class \code{'agEvaluate'}.
 #'  
-#' @param pmats \code{list}; A nested list of dimension D x M x P x G x K (result of \code{performance.R}), where the terminal node is a performance matrix.
+#' @param pf \code{list}; A nested list of dimension D x M x P x G x K (result of \code{performance.R}), where the terminal node is a performance matrix.
 #' 
 #' @details The statistics provided in the output are as follows:
 #' \itemize{
@@ -20,9 +20,9 @@
 #' \item \code{(dip)}; p-value of dip test for unimodality (see \code{?dip} for details)
 #' }
 
-agEvaluate <- function(pmats){
+agEvaluate <- function(pf){
   
-  if(class(pmats) != "pmat") stop("'pmats' object must be of class 'pmat'. Use performance() to generate such objects.")
+  if(class(pf) != "pf") stop("'pf' object must be of class 'pf'. Use performance() to generate such objects.")
 
     skew <- function(x, na.rm = TRUE){
     stopifnot(is.numeric(x))
@@ -39,12 +39,12 @@ agEvaluate <- function(pmats){
     return(sk)
   }
 
-  D <- length(pmats)
-  M <- length(pmats[[1]])
-  P <- length(pmats[[1]][[1]])
-  G <- length(pmats[[1]][[1]][[1]])
-  K <- length(pmats[[1]][[1]][[1]][[1]])
-  C <- length(pmats[[1]][[1]][[1]][[1]][[1]])
+  D <- length(pf)
+  M <- length(pf[[1]])
+  P <- length(pf[[1]][[1]])
+  G <- length(pf[[1]][[1]][[1]])
+  K <- length(pf[[1]][[1]][[1]][[1]])
+  C <- length(pf[[1]][[1]][[1]][[1]][[1]])
   
   dataset <- 1:D
   
@@ -59,8 +59,8 @@ agEvaluate <- function(pmats){
   gap_vec_names <- numeric(G)
   method_names <- character(M)
   
-  prop_vec <- as.numeric(gsub("p","",names(pmats[[1]][[1]])))
-  gap_vec <- as.numeric(gsub("g","",names(pmats[[1]][[1]][[1]])))
+  prop_vec <- as.numeric(gsub("p","",names(pf[[1]][[1]])))
+  gap_vec <- as.numeric(gsub("g","",names(pf[[1]][[1]][[1]])))
   
   
   for(d in 1:D){
@@ -69,47 +69,47 @@ agEvaluate <- function(pmats){
       for(g in 1:G){
         gap_vec_names[g] <- c(paste("g", gap_vec[g],sep="")) # vector of names
         for(m in 1:M){
-          method_names[m] <- names(pmats[[1]])[m]
+          method_names[m] <- names(pf[[1]])[m]
           
             # compute the mean and distribution of the performance criteria in each (d,m,p,g) specification across all k pairs of (x,X) and 
             # store results in a list of data frames
           
-            quantiles <- apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1, 
+            quantiles <- apply(sapply(pf[[d]][[m]][[p]][[g]],unlist),1, 
                                FUN=function(x) quantile(x, probs = c(0, 0.025, 0.25, 0.75, 0.975, 1.0), na.rm = TRUE))
             
             Evaluation[[d]][[p]][[g]][[m]] <- data.frame(
               
-              mean = rowMeans(sapply(pmats[[d]][[m]][[p]][[g]],unlist), na.rm = TRUE),
+              mean = rowMeans(sapply(pf[[d]][[m]][[p]][[g]],unlist), na.rm = TRUE),
               
-              sd = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,sd, na.rm = TRUE),
+              sd = apply(sapply(pf[[d]][[m]][[p]][[g]],unlist),1,sd, na.rm = TRUE),
               
-              #q0 = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,quantile, na.rm = TRUE)["0%",],
+              #q0 = apply(sapply(pf[[d]][[m]][[p]][[g]],unlist),1,quantile, na.rm = TRUE)["0%",],
               q0 = quantiles["0%",],
               
-              #q2.5 = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1, 
+              #q2.5 = apply(sapply(pf[[d]][[m]][[p]][[g]],unlist),1, 
                            #FUN=function(x) quantile(x, probs = c(0.025,0.975), na.rm = TRUE))["2.5%",],
               q2.5 = quantiles["2.5%",],
               
-              #q25 = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,quantile, na.rm = TRUE)["25%",],
+              #q25 = apply(sapply(pf[[d]][[m]][[p]][[g]],unlist),1,quantile, na.rm = TRUE)["25%",],
               q25 = quantiles["25%",],
               
-              median = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,median, na.rm = TRUE),
+              median = apply(sapply(pf[[d]][[m]][[p]][[g]],unlist),1,median, na.rm = TRUE),
               
-              #q75 = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,quantile, na.rm = TRUE)["75%",],
+              #q75 = apply(sapply(pf[[d]][[m]][[p]][[g]],unlist),1,quantile, na.rm = TRUE)["75%",],
               q75 = quantiles["75%",],
               
-              #q97.5 = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1, 
+              #q97.5 = apply(sapply(pf[[d]][[m]][[p]][[g]],unlist),1, 
                             #FUN=function(x) quantile(x, probs = c(0.025,0.975), na.rm = TRUE))["97.5%",],
               q97.5 = quantiles["97.5%",],
               
-              #q100 = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,quantile, na.rm = TRUE)["100%",],
+              #q100 = apply(sapply(pf[[d]][[m]][[p]][[g]],unlist),1,quantile, na.rm = TRUE)["100%",],
               q100 = quantiles["100%",],
               
               iqr = quantiles["75%",] - quantiles["25%",],
               
-              skewness = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,skew), 
+              skewness = apply(sapply(pf[[d]][[m]][[p]][[g]],unlist),1,skew), 
               
-              dip = apply(sapply(pmats[[d]][[m]][[p]][[g]],unlist),1,
+              dip = apply(sapply(pf[[d]][[m]][[p]][[g]],unlist),1,
                           FUN = function(x){dip.test(x,simulate.p.value = TRUE)$p.value
                             
               }),
@@ -128,7 +128,7 @@ agEvaluate <- function(pmats){
     }
     names(Evaluation[[d]]) <- prop_vec_names 
   }
-  names(Evaluation) <- names(pmats)
+  names(Evaluation) <- names(pf)
   
   class(Evaluation) <- "agEvaluate"
     return(Evaluation)
