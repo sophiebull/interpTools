6ff8be9c329fc796ccb7d911eb055f9492ce8395
Sophie Castel <sophiecastel@trentu.ca>
2020-08-04 15:43:04 -0400

    n (deprecated) ----> N
    fixed arima.sim so that output is numeric instead of ts

--- R/simWt.R
+++ R/simWt.R
@@ -1,21 +1,21 @@
 #' Simulate W_t
 #' 
 #' Function to simulate W_t: the noise component of X_t. Defaults to white noise. 
-#' @param n The length of the output series 
+#' @param N The length of the output series 
 #' @param p The AR order
 #' @param q The MA order
 #' @param var The variance of the stochastic process
 
-simWt <- function(n = 1000, p = 0, q = 0, var = 1){
+simWt <- function(N = 1000, p = 0, q = 0, var = 1){
   
-  if(!is.numeric(n)) stop("n must be a numeric quantity >=10.")
+  if(!is.numeric(N)) stop("N must be a numeric quantity >=10.")
   if(!is.numeric(p)) stop("p must be a numeric quantity >=0.")
   if(!is.numeric(q)) stop("q must be a numeric quantity >=0.")
   if(!is.numeric(var)) stop("var must be a numeric quantity >0.")
     
     
   Wt_list <- list()
-  stopifnot(p >= 0,q >= 0,n >= 10, n %% 1 == 0, var > 0)
+  stopifnot(p >= 0,q >= 0,N >= 10, N %% 1 == 0, var > 0)
   
   if(p > 0){
     repeat{
@@ -50,7 +50,7 @@ simWt <- function(n = 1000, p = 0, q = 0, var = 1){
     }
   }
   
-  Wt_list$value = arima.sim(model, n = n, sd = sqrt(var))
+  Wt_list$value = as.numeric(arima.sim(model, n = N, sd = sqrt(var)))
   Wt_list$p = p
   Wt_list$q = q
   return(Wt_list)
