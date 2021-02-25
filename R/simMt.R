61f16c9ad499749bf2aeb74c0c4c2348bab4a40b
Sophie Castel <sophiecastel@trentu.ca>
2021-02-25 14:21:46 -0500

    Removed 'trendType' feature - focused heavily on improving polynomial definition. User can now customize the polynomial through specification of the polynomial coefficients, centering parameters, and grand mean.

--- R/simMt.R
+++ R/simMt.R
@@ -3,67 +3,97 @@
 #' Function to simulate M_t: the mean component of X_t. 
 #' @param N The length of the output series
 #' @param numTrend The number of terms to include in the varying trend component of M_t. Value represents the maximum degree; all lower order terms included.
-#' @param trendType The type of trends to include. Either "polynomial" or "exponential".
  
-simMt <- function(N = 1000, numTrend = 0, trendType = "polynomial"){
+simMt <- function(N = 1000, numTrend = 0, a = NULL, mu = NULL, center = NULL){
   
-  if(trendType != "polynomial" && trendType != "exponential"){
-    warning("Please specify trendType as either 'polynomial' or 'exponential'.")
-    stop()
+  stopifnot(is.numeric(N), N>=10, is.numeric(numTrend), numTrend>=0)
+  
+  if(any(is.null(a), is.null(mu), is.null(center))){
+    nulls <- c(a = is.null(a), mu = is.null(mu), center = is.null(center))
+    warning(c("Value(s): '", paste0(names(nulls)[which(nulls)], sep = ", "), "' unspecified. Defaulting to random generation."))
   }
-  
-  stopifnot(is.numeric(N), N>=10, is.numeric(numTrend), numTrend>=0, (trendType == "polynomial" | trendType == "exponential"))
-  
   
   Mt_list <- list()
   t <- 0:(N-1)
+  mut <- numeric(numTrend) # Placeholder vector for each term in the full model 
+
+  ##########################
+  # POLYNOMIAL COEFFICIENTS
+  ##########################
   
-  ######### Initializing Mt 
-  mu <- runif(1, -N/100, N/100)
-  mut <- numeric(numTrend)
-  
-  if(trendType == "polynomial"){
-    
+  if(is.null(a)){
     if(numTrend > 0){
-      center <- sample(1:N, 1)
-      if(numTrend > 1){
-        for(k in 1:(numTrend-1)){
-          a <- rnorm(1, mean = 0, sd = N/(20*k))
-          mut[k] <- paste("(",a,")*((t-center)/N)^",k,"+",sep="")
-        }
+      a <- numeric(numTrend)
+      for(k in 1:numTrend){
+        a[k] <- rnorm(1, mean = 0, sd = N/(20*k)) # randomly generate coefficient for each term 
       }
-      
-      a <- rnorm(1, mean = 0, sd = N/(20))
-      mut[numTrend] <- paste("(",a,")*((t-center)/N)^",numTrend,sep="")
-      Mt_0 = NULL
     }
-    
     if(numTrend == 0){
-      mut = 0
+      a = 0
+    }
+    warning(c("Polynomial coefficient(s) = ", paste0(round(a,3), sep = " ")))
+  }
+  
+  else if(!is.null(a)){
+    if(length(a) != numTrend){
+        
+      missing <- rep(NA, numTrend)
+      missing[1:length(a)] <- a
+        
+      stop(c("Polynomial coefficient(s): ", paste0("a_", which(is.na(missing)), sep = ", ") , "not specified. 
+             Length of 'a' must be equal to the number of terms in the polynomial (numTrend = ", paste0(numTrend, ").")))
     }
   }
   
-  if(trendType == "exponential"){
-    if(numTrend > 0){
-      if(numTrend > 1){
-        for(k in 1:(numTrend-1)){
-          a <- sample(-(N/10):(N/10),1)
-          mut[k] <- paste("(",a,")*exp(",k,"*(t/N))+",sep="")
-        }
-      }
-      
-      a <- sample(-(N/10):(N/10),1)
-      mut[numTrend] <- paste("(",a,")*exp(",numTrend,"*(t/N))",sep="")
-      Mt_0 = NULL
-    }
-    
+  ###########################
+  # CENTERING PARAMETER
+  ###########################
+  
+  if(is.null(center)){
+    center <- sample(1:N, 1) # randomly generate common centering parameter for all terms
     if(numTrend == 0){
-      mut = 0
+      center <- 0
     }
+    warning(paste0("Centering parameter = ", center))
+  }
+     
+  else if(!is.null(center)){ 
+    if(length(center) != numTrend){
+        
+      missing <- rep(NA, numTrend)
+      missing[1:length(center)] <- center
+        
+      stop(c("Centering parameter(s): ", paste0("c_", which(is.na(missing)), sep = ", ") , "not specified. 
+             Length of 'center' must be equal to the number of terms in the polynomial (numTrend = ", paste0(numTrend, ").")))
+    }
+  }
+  
+  ######################
+  # GRAND MEAN 
+  ######################
+  
+  if(is.null(mu)){
+    mu <- runif(1, -N/100, N/100) # randomly generate grand mean
+    warning(paste0("mu = ", round(mu,3)))
+  } 
+  
+  else if(!is.null(mu)){
+    if(length(mu) != 1){
+      stop("'mu' must be a single value representing the grand mean.")
+    }
+  }
+      
+  if(numTrend > 0){
+    mut <- paste("(",a,")*((t-", center,")/N)^",1:numTrend," +", sep = "", collapse = "")
+    mut <- gsub(".{1}$", "", mut)
+  }
+ 
+  else if(numTrend == 0){
+    mut = 0
   }
   
   mut_fn <- mut
-  Mt_fn <- paste(c(mut_fn,"+",mu),collapse="")
+  Mt_fn <- paste(c(mut_fn,"+",mu),collapse=" ")
   
   mut <- eval(parse(text = paste(mut,collapse="")))
   Mt <- mut+mu
