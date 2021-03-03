79c715410c02b69d606835695ae83eaa0f7329cd
Sophie Castel <sophiecastel@trentu.ca>
2021-03-03 14:03:40 -0500

    DEPRECATED: agEval ----> agObject

--- R/compileMatrix.R
+++ R/compileMatrix.R
@@ -1,29 +1,32 @@
-#'  Compile agEvaluate Objects into Matrices
+#'  Compile 'aggregate' Objects into Matrices
 #' 
-#'  A function to compile objects of class "agEvaluate" (from agEvaluate()) into matrices of dimension PxG.\cr
-#'  Elements of agEvaluate are arranged into this matrix corresponding to (p,g).
+#'  A function to compile objects of class \code{"aggregate"} (from \code{aggregate()}) into matrices of dimension PxG.\cr
+#'  Elements of \code{aggregate} are arranged into this matrix corresponding to (p,g).
 #'  
-#' @param agEval A list object (result of agEvaluate.R) of aggregated performance metrics
+#' @param agObject A list object (result of \code{aggregate()}) of aggregated performance metrics
 #' 
 
-compileMatrix <-  function(agEval){ 
+compileMatrix <-  function(agObject){ 
   
-D <- length(agEval)  
-P <- length(agEval[[1]])
-G <- length(agEval[[1]][[1]])
-M <- length(agEval[[1]][[1]][[1]])
-C <- nrow(agEval[[1]][[1]][[1]][[1]])
-stat <- c("mean","sd","q0","q2.5","q25","median","q75","q97.5","q100","iqr","skewness","dip")
+D <- length(agObject)  
+P <- length(agObject[[1]])
+G <- length(agObject[[1]][[1]])
+M <- length(agObject[[1]][[1]][[1]])
+C <- nrow(agObject[[1]][[1]][[1]][[1]])
+stat <- names(agObject[[1]][[1]][[1]][[1]])
 
-prop_vec_names <- names(agEval[[1]])
-gap_vec_names <- names(agEval[[1]][[1]])
+exclude <- c("gap_width", "prop_missing", "dataset","method") 
+stat <- stat[!stat %in% exclude] # Exclude metadata
+
+prop_vec_names <- names(agObject[[1]])
+gap_vec_names <- names(agObject[[1]][[1]])
 
 theVals <- lapply(theVals <- vector(mode = 'list', length(stat)),function(x)
   lapply(theVals <- vector(mode = 'list', C),function(x) 
     lapply(theVals <- vector(mode = 'list', M),function(x) 
       x <- vector(mode = 'list', D))))
 
-critMat <- matrix(nrow=length(agEval[[1]]),ncol=length(agEval[[1]][[1]]))
+critMat <- matrix(nrow=length(agObject[[1]]),ncol=length(agObject[[1]][[1]]))
 rownames(critMat) <- prop_vec_names
 colnames(critMat) <- gap_vec_names
 
@@ -36,8 +39,8 @@ method_list_names <- numeric(M)
         for(d in 1:D){
           for(p in 1:P){
             for(g in 1:G){
-              critMat[p,g] <- agEval[[d]][[p]][[g]][[m]][s,stat[st]]
-              method_list_names[m] <- as.character(agEval[[d]][[p]][[g]][[m]][s, "method"]) 
+              critMat[p,g] <- agObject[[d]][[p]][[g]][[m]][s,stat[st]]
+              method_list_names[m] <- as.character(agObject[[d]][[p]][[g]][[m]][s, "method"]) 
             }
           }
           theVals[[st]][[s]][[m]][[d]] <- critMat
@@ -47,7 +50,7 @@ method_list_names <- numeric(M)
       }
       names(theVals[[st]][[s]]) <- method_list_names
     }
-    names(theVals[[st]]) <- rownames(agEval[[d]][[p]][[g]][[m]])
+    names(theVals[[st]]) <- rownames(agObject[[d]][[p]][[g]][[m]])
   }
 
   names(theVals) <- stat
