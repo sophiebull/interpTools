ca2b66a00e3eddee9348c40f2629dbb5a03f64be
Sophie Castel <sophiecastel@trentu.ca>
2021-03-03 14:09:45 -0500

    DEPRECATED: crit ------> metric agEval ------> agObject

--- R/multiSurface.R
+++ R/multiSurface.R
@@ -9,19 +9,19 @@
 #' 
 #' @param d \code{numeric}; A vector to indicate datasets of interest
 #' @param m \code{character}; A vector of interpolation methods of interest
-#' @param crit \code{character}; An element describing the performance metric of interest
-#' @param agEval \code{agEvaluate}; An object containing the aggregated performance metrics (result of \code{agEvaluate()})
-#' @param layer_type \code{character}; How to layer the surfaces (by "method" or by "dataset") 
-#' @param f \code{character}; The statistic of interest defining the surface \code{f(p,g)}. Possible choices are listed in \code{?agEvaluate}.
+#' @param metric \code{character}; An element describing the performance metric of interest
+#' @param agObject \code{agObject}; An object containing the aggregated performance metrics (result of \code{aggregate()})
+#' @param layer_type \code{character}; How to layer the surfaces (by \code{"method"} or by \code{"dataset"}) 
+#' @param f \code{character}; The statistic of interest defining the surface \code{f(p,g)}. Possible choices are listed in \code{?aggregate}.
 #' @param highlight \code{character/numeric}; A single method (if \code{layer_type = "method"}) or dataset (if \code{layer_type = "dataset"}) to highlight.
 #' @param highlight_color \code{character}; An HTML color of format \code{"#xxxxxx"} to apply to \code{highlight}
 #' @param colors \code{character}; A vector of the desired color palette, with entries in HTML format (\code{"#xxxxxx"}) 
 #' 
 
-multiSurface <- function(d = 1:length(agEval),
-                         m = names(agEval[[1]][[1]][[1]]),
-                         crit,
-                         agEval, 
+multiSurface <- function(d = 1:length(agObject),
+                         m = names(agObject[[1]][[1]][[1]]),
+                         metric,
+                         agObject, 
                          layer_type = "method",
                          f = "median",
                          highlight = "HWI",
@@ -34,12 +34,12 @@ multiSurface <- function(d = 1:length(agEval),
   if(sum(duplicated(d) != 0)) stop(paste0("'d' contains redundant elements at position(s): ", paste0(c(1:length(d))[duplicated(d)], collapse = ", ") ))
   if(sum(duplicated(m) != 0)) stop(paste0("'m' contains redundant elements at position(s): ", paste0(c(1:length(m))[duplicated(m)], collapse = ", ") ))
   
-  if(!all(m %in%  names(agEval[[1]][[1]][[1]]))) stop("Method(s) '", paste0(m[!m %in% names(agEval[[1]][[1]][[1]])], collapse = ", ' "),"' not found. Possible choices are: '", paste0(names(agEval[[1]][[1]][[1]]), collapse = "', '"),"'.")
-  if(!all(paste0("D",d) %in% names(agEval))) stop("Dataset(s) ", paste0(d[!paste0("D",d) %in% names(agEval)], collapse = ", ")," not found. Possible choices are: ", paste0(gsub("D", "",names(agEval)), collapse = ", "))
-  if(!all(f %in% names(agEval[[1]][[1]][[1]][[1]]))) stop(paste0(c("f must be one of: '",paste0(names(agEval[[1]][[1]][[1]][[1]]), collapse = "', '"),"'."), collapse = ""))
-  if(!crit %in% rownames(agEval[[1]][[1]][[1]][[1]])) stop(paste0("Criterion '",crit,"' must be one of ", paste(rownames(agEval[[1]][[1]][[1]][[1]]),collapse = ", "),"."))
+  if(!all(m %in%  names(agObject[[1]][[1]][[1]]))) stop("Method(s) '", paste0(m[!m %in% names(agObject[[1]][[1]][[1]])], collapse = ", ' "),"' not found. Possible choices are: '", paste0(names(agObject[[1]][[1]][[1]]), collapse = "', '"),"'.")
+  if(!all(paste0("D",d) %in% names(agObject))) stop("Dataset(s) ", paste0(d[!paste0("D",d) %in% names(agObject)], collapse = ", ")," not found. Possible choices are: ", paste0(gsub("D", "",names(agObject)), collapse = ", "))
+  if(!all(f %in% names(agObject[[1]][[1]][[1]][[1]]))) stop(paste0(c("f must be one of: '",paste0(names(agObject[[1]][[1]][[1]][[1]]), collapse = "', '"),"'."), collapse = ""))
+  if(!metric %in% rownames(agObject[[1]][[1]][[1]][[1]])) stop(paste0("Metric '",metric,"' must be one of ", paste(rownames(agObject[[1]][[1]][[1]][[1]]),collapse = ", "),"."))
   
-  if(length(crit) != 1) stop("'crit' must contain only a single character element.")
+  if(length(metric) != 1) stop("'metric' must contain only a single character element.")
   if(length(f) != 1) stop("'f' must contain only a single character element.")
   if(length(layer_type) != 1) stop("'layer_type' must contain only a single character element.")
   if(length(highlight) != 1) stop("'highlight' must contain only a single character element.")
@@ -47,7 +47,7 @@ multiSurface <- function(d = 1:length(agEval),
   
   if(layer_type != "method" & layer_type != "dataset") stop("'layer_type' must equal either 'method' or 'dataset'.")
   
-  if(class(agEval) != "agEvaluate") stop("'agEval' object must be of class 'agEvaluate'. Please use agEvaluate().")
+  if(class(agObject) != "aggregate") stop("'agObject' object must be of class 'aggregate'. Please use aggregate().")
   
   if(layer_type == "method" & !highlight %in% m) stop(paste0(c("'highlight' must be an element of 'm'. Choose one of: '", paste0(m, collapse = "', '"),"'."), collapse = ""))
   if(layer_type == "dataset" & !highlight %in% d) stop(paste0(c("'highlight' must be an element of 'd'. Choose one of: '", paste0(d, collapse = "', '"),"'."), collapse = ""))
@@ -57,11 +57,11 @@ multiSurface <- function(d = 1:length(agEval),
   if(layer_type == "dataset" & !is.numeric(highlight)) stop("If 'layer_type' = 'dataset', then 'highlight' must be of class 'numeric'.")
   if(layer_type == "method" & !is.character(highlight)) stop("If 'layer_type' = 'method', then 'highlight' must be of class 'character'.")
   
-  if(layer_type == "method" & length(d) < 2) stop("Only 1 dataset was chosen. Please specify at least one more, or use plotSurface() instead.") 
-  if(layer_type == "dataset" & length(m) < 2) stop("Only 1 method was chosen. Please specify at least one more, or use plotSurface() instead.") 
+  #if(layer_type == "method" & length(d) < 2) stop("Only 1 dataset was chosen. Please specify at least one more, or use plotSurface() instead.") 
+  #if(layer_type == "dataset" & length(m) < 2) stop("Only 1 method was chosen. Please specify at least one more, or use plotSurface() instead.") 
   
-  if(layer_type == "method" & length(d) < 3){ warning("multiSurface() layout works best with at least three plots. Next time, specify at least 3 elements in 'd'.")}
-  if(layer_type == "dataset" & length(m) < 3){ warning("multiSurface() layout works best with at least three plots. Next time, specify at least 3 elements in 'm'.")}
+  #if(layer_type == "method" & length(d) < 3){ warning("multiSurface() layout works best with at least three plots. Next time, specify at least 3 elements in 'd'.")}
+  #if(layer_type == "dataset" & length(m) < 3){ warning("multiSurface() layout works best with at least three plots. Next time, specify at least 3 elements in 'm'.")}
   
   D <- length(d)
   M <- length(m)
@@ -69,7 +69,7 @@ multiSurface <- function(d = 1:length(agEval),
   ##################
   
   # BUILD LEGEND ###########################################################################
-  z_list <- compileMatrix(agEval=agEval)[[f]]
+  z_list <- compileMatrix(agObject=agObject)[[f]]
   
   data_list_names = names(z_list[[1]][[1]])[d]
   method_list_names <- names(z_list[[1]])[names(z_list[[1]]) %in% m]
@@ -133,8 +133,8 @@ multiSurface <- function(d = 1:length(agEval),
   
   # GENERATE PLOTS #######################################################################################
   
-  ps <- plotSurface(crit=crit, 
-                    agEval = agEval, 
+  ps <- plotSurface(metric=metric, 
+                    agObject = agObject, 
                     layer_type = layer_type,
                     f = f,
                     m = m, 
@@ -199,7 +199,7 @@ multiSurface <- function(d = 1:length(agEval),
   scene_string <- c("scene",scene_string)
   
   
-  l_string <- paste0("layout(title = crit,")
+  l_string <- paste0("layout(title = metric,")
   
   l_string <- c(l_string, paste0(scene_string[1:oth_bound]," = list(domain = list(x = c(",x1[1:oth_bound],",",x2[1:oth_bound],"), y = c(",y1[1:oth_bound],",",y2[1:oth_bound],")),
                                  aspectmode = 'manual',
@@ -208,6 +208,6 @@ multiSurface <- function(d = 1:length(agEval),
   l_string <- c(l_string, paste0(scene_string[oth_bound+1], " = list(domain = list(x = c(",x1[oth_bound+1],",",x2[oth_bound+1],"), y = c(",y1[oth_bound+1],",",y2[oth_bound+1],"))))"))
 
   call <- paste0(c(p_string,l_string))
-  
+
   return(eval(parse(text = call)))
 }
