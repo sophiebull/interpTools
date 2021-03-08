1bee2f47cb462687621d47bed62b2abcbfa5712e
Sophie Castel <sophiecastel@trentu.ca>
2021-03-07 19:39:34 -0500

    DEPRECATED: agEval ---> agObject, agEvaluate() ----> aggregate() and crit---->metric
    Fixed bug with redundant object naming (symmetric) ----> show_symmetric

--- R/plotSkew.R
+++ R/plotSkew.R
@@ -1,26 +1,26 @@
 #' Aggregate and plot the skewness statistics over all simulations
 #' 
-#' Function to display the sampling distributions of the skewness statistics for each criterion across all simulations. 
-#' Sampling distributions with a sample mean roughly centered at zero indicate that on average the criterion has a reliably 
+#' Function to display the sampling distributions of the skewness statistics for each metric across all simulations. 
+#' Sampling distributions with a sample mean roughly centered at zero indicate that on average the metric has a reliably 
 #' symmetric distribution.  Distributions of skewness values whose center is not in the neighbourhood of zero (-s/3 <= Xbar <= s/3) indicate that 
-#' on average the criterion has a skewed distribution, and the median will better capture the essence of the data.
+#' on average the metric has a skewed distribution, and the median will better capture the essence of the data.
 #' 
-#' @param agEval A list object (result of agEval.R) of aggregated performance metrics
-#' @param cptwise logical; Whether to display plots individually (histograms) or together as a density plot
-#' @param symmetric logical; TRUE = Display only symmetric criteria, FALSE = Display only asymmetric criteria, NULL = Display all criteria
-#' @param output character; "plots" (default) or "table".
+#' @param agObject \code{aggregate}; A list object (result of \code{aggregate.R}) of aggregated performance metrics
+#' @param cptwise \code{logical}; Whether to display plots individually (histograms) or together as a density plot
+#' @param symmetric \code{logical}; TRUE = Display only symmetric metrics, FALSE = Display only asymmetric metrics, NULL = Display all metrics
+#' @param output \code{character}; "plots" (default) or "table".
 
-plotSkew <- function(agEval, cptwise=F, symmetric= NULL, output = "plots", crit = rownames(agEval[[1]][[1]][[1]][[1]])){
+plotSkew <- function(agObject, cptwise=F, show_symmetric= NULL, output = "plots", metric = rownames(agObject[[1]][[1]][[1]][[1]])){
   
-  D <- length(agEval)
-  P <- length(agEval[[1]])
-  G <- length(agEval[[1]][[1]])
-  M <- length(agEval[[1]][[1]][[1]])
-  C <- nrow(agEval[[1]][[1]][[1]][[1]][crit,])
+  D <- length(agObject)
+  P <- length(agObject[[1]])
+  G <- length(agObject[[1]][[1]])
+  M <- length(agObject[[1]][[1]][[1]])
+  C <- nrow(agObject[[1]][[1]][[1]][[1]][metric,])
 
-  stopifnot((is.logical(cptwise) | is.logical(symmetric)), class(agEval) == "agEvaluate")
+  stopifnot((is.logical(cptwise) | is.logical(show_symmetric)), class(agObject) == "aggregate")
   
-  skews <- matrix(ncol = D*P*G*M, nrow = nrow(agEval[[1]][[1]][[1]][[1]][crit,]))
+  skews <- matrix(ncol = D*P*G*M, nrow = nrow(agObject[[1]][[1]][[1]][[1]][metric,]))
   q1s <- skews
   q3s <- q1s
   
@@ -46,9 +46,9 @@ plotSkew <- function(agEval, cptwise=F, symmetric= NULL, output = "plots", crit 
       for(g in 1:G){
         for(m in 1:M){
           
-          skewcol <- agEval[[d]][[p]][[g]][[m]][crit,"skewness"]
-          q1col <- agEval[[d]][[p]][[g]][[m]][crit,"q25"]
-          q3col <- agEval[[d]][[p]][[g]][[m]][crit,"q75"]
+          skewcol <- agObject[[d]][[p]][[g]][[m]][metric,"skewness"]
+          q1col <- agObject[[d]][[p]][[g]][[m]][metric,"q25"]
+          q3col <- agObject[[d]][[p]][[g]][[m]][metric,"q75"]
           skews[,i] <- skewcol
           q1s[,i] <- q1col
           q3s[,i] <- q3col
@@ -59,8 +59,8 @@ plotSkew <- function(agEval, cptwise=F, symmetric= NULL, output = "plots", crit 
     }
   }
   
-  names <- rownames(agEval[[1]][[1]][[1]][[1]])
-  names <- names[which(names %in% crit)]
+  names <- rownames(agObject[[1]][[1]][[1]][[1]])
+  names <- names[which(names %in% metric)]
   
   rownames(skews) = names
   rownames(q1s) = names
@@ -75,31 +75,31 @@ plotSkew <- function(agEval, cptwise=F, symmetric= NULL, output = "plots", crit 
   
   gather <- gather(skews)
   
-  symCrit <- rownames(skewMeans[which(skewMeans$value >= -sd(skewMeans$value)/3 & skewMeans$value <= sd(skewMeans$value)/3),])
-  symCritIn <-match(symCrit,rownames(skewMeans))
+  symmetric <- rownames(skewMeans[which(skewMeans$value >= -sd(skewMeans$value)/3 & skewMeans$value <= sd(skewMeans$value)/3),])
+  symmetricIn <-match(symmetric,rownames(skewMeans))
 
   skewCols <- data.frame(key = colnames(skews), value = rep("white",length(colnames(skews))))
   skewCols$value = as.character(skewCols$value)
-  skewCols[symCritIn,]$value = c("grey63")
+  skewCols[symmetricIn,]$value = c("grey63")
   
-  sym <- gather[gather$key %in% symCrit,]
-  asym <- gather[!(gather$key %in% symCrit),]
+  sym <- gather[gather$key %in% symmetric,]
+  asym <- gather[!(gather$key %in% symmetric),]
   
   skewPlot <- list()
   
   if(cptwise){
-    if(is.null(symmetric)){
+    if(is.null(show_symmetric)){
       Cc = c(1:C)
       my.data = gather
     }
     
-    else if(symmetric){
-      Cc = c(1:C)[symCritIn]
+    else if(show_symmetric){
+      Cc = c(1:C)[symmetricIn]
       my.data = sym
     }
     
-    else if(!symmetric){
-      Cc = c(1:C)[-symCritIn]
+    else if(!show_symmetric){
+      Cc = c(1:C)[-symmetricIn]
       my.data = asym
     }
     
@@ -109,8 +109,8 @@ plotSkew <- function(agEval, cptwise=F, symmetric= NULL, output = "plots", crit 
       
       
       my.data$colour = rep("white",nrow(my.data))
-      my.data$colour[!my.data$key %in% symCrit] = "white"
-      my.data$colour[my.data$key %in% symCrit] = "grey"
+      my.data$colour[!my.data$key %in% symmetric] = "white"
+      my.data$colour[my.data$key %in% symmetric] = "grey"
     
       thePlot <- ggplot(my.data, aes(x = value)) + 
         theme_light() + 
@@ -133,32 +133,32 @@ plotSkew <- function(agEval, cptwise=F, symmetric= NULL, output = "plots", crit 
   }
   
   else if(!cptwise){
-    if(!is.null(symmetric)){
-      if(symmetric){
+    if(!is.null(show_symmetric)){
+      if(show_symmetric){
       thePlot <- ggplot(sym) + geom_density(aes(x = sym$value, color = sym$key), lwd = 0.25) + theme_light() + 
-        xlim(-5,5) + xlab("skewness") + labs(color = "criteria") + 
-        scale_colour_manual(values = colorRampPalette(c("blue","pink","turquoise"))(length(symCrit))) #tidyr
+        xlim(-5,5) + xlab("skewness") + labs(color = "metriceria") + 
+        scale_colour_manual(values = colorRampPalette(c("blue","pink","turquoise"))(length(symmetric))) #tidyr
       }
-      else if(!symmetric){
+      else if(!show_symmetric){
       thePlot <- ggplot(asym) + geom_density(aes(x = asym$value, color = asym$key), lwd = 0.25) + theme_light() + 
-        xlim(-5,5) + xlab("skewness") + labs(color = "criteria") + 
-        scale_colour_manual(values = colorRampPalette(c("blue","pink","turquoise"))(C-length(symCrit))) #tidyr
+        xlim(-5,5) + xlab("skewness") + labs(color = "metriceria") + 
+        scale_colour_manual(values = colorRampPalette(c("blue","pink","turquoise"))(C-length(symmetric))) #tidyr
       }
     }
     
-    else if(is.null(symmetric)){
+    else if(is.null(show_symmetric)){
       thePlot <- ggplot(gather) + geom_density(aes(x = gather$value, color = gather$key), lwd = 0.25) + theme_light() + 
-        xlim(-5,5) + xlab("skewness") + labs(color = "criteria") + 
+        xlim(-5,5) + xlab("skewness") + labs(color = "metriceria") + 
         scale_colour_manual(values = colorRampPalette(c("blue","pink","turquoise"))(C)) #tidyr
     }
   }
 
     vals <- format(round(skewMeans$value,2),nsmall = 2)
     
-    vals[symCritIn] <- paste0("\\textbf{", vals[symCritIn], "}") # bold non-skewed criteria
+    vals[symmetricIn] <- paste0("\\textbf{", vals[symmetricIn], "}") # bold non-skewed metriceria
     
     theTable <- data.frame(rownames(skewMeans),vals)
-    colnames(theTable) <- c("criterion","mean skewness")
+    colnames(theTable) <- c("metricerion","mean skewness")
     
     if(output == "plots"){
       return(thePlot)
